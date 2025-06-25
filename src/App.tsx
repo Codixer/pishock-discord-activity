3e87334782d39df2e99f4876ef3f0f4eb1e77d1f
Stefano <stefano@stefanocoding.me>
2025-06-25 15:33:12 +0200

    Add entitlements scope to Discord authorization

--- src/App.tsx
+++ src/App.tsx
@@ -603,6 +603,7 @@ function MainApp() {
               'guilds',
               'guilds.members.read',
               'rpc.activities.write',
+             'entitlements',
             ],
           });
 
